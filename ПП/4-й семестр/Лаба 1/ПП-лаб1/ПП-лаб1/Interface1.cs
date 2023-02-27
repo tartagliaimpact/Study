437482637b2fbc5b1eec2c6e855667b7efcf4556
tartagliaimpact <96656441+tartagliaimpact@users.noreply.github.com>
2023-02-28 00:37:55 +0300

    Add files via upload

---
+++ ПП/4-й семестр/Лаба 1/ПП-лаб1/ПП-лаб1/Interface1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace лаб1_ООП
+{
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    partial class Program
+    {
+        public delegate void Delegat(string s);
+
+        interface I1
+        {
+            public int count { get; }
+            public void Count();
+
+            public event Delegat Event;
+
+            public int this[int i] { get; set; }
+        }
+
+    }
+}
---
+++ ПП/4-й семестр/Лаба 1/ПП-лаб1/ПП-лаб1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаб1_ООП
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            C1 obj1 = new C1();
+            C1 obj2 = new C1(1, 2, 3, 4, 'd', 5.0F);
+            C1 obj3 = new C1(obj2);
+
+            Console.Write(obj2.SumIntFields());
+            Console.WriteLine();
+            obj1.Show();
+            Console.WriteLine();
+            obj2.Show();
+            Console.WriteLine();
+            obj3.Show();
+            Console.WriteLine();
+
+            C2 obj4 = new C2();
+            C2 obj5 = new C2(1, 2, 3, 4, 'd', 5.0F);
+            obj5.Count();
+
+            Worker worker1 = new Worker("Tom", 21, 600);
+            worker1.Print();
+            Console.WriteLine();
+            worker1.PrintInfo();
+
+            Console.WriteLine();
+
+            Person person1 = new Person("Aki", 19);
+            Worker worker2 = new Worker(person1.Name, person1.Age, 500);
+            worker2.Print();
+            Console.WriteLine();
+            worker2.PrintInfo();
+        }
+    }
+}
