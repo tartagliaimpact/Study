68062b8588f8dd6618f698091b4e5141a6786299
tartagliaimpact <96656441+tartagliaimpact@users.noreply.github.com>
2023-02-28 00:22:48 +0300

    Add files via upload

---
+++ ООП/3-й семестр/lab14v6-OOP/lab14v6-OOP/Program.cs
@@ -0,0 +1,187 @@
+﻿using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            foreach (Process process in Process.GetProcesses())
+            {
+
+                try
+                {
+                    Console.WriteLine($"ID: {process.Id}");
+                    Console.WriteLine($"Имя: { process.ProcessName} ");
+                    Console.WriteLine($"Используемая физ память: {process.WorkingSet64}");
+                    Console.WriteLine($"Базовый приоритет: {process.BasePriority}");
+                    Console.WriteLine($"Размер виртуальной памяти: {process.VirtualMemorySize64}");
+                    Console.WriteLine($"Время запуска: {process.StartTime}");
+                    Console.WriteLine($"Получает пользовательское время процессора для этого процесса: {process.UserProcessorTime}");
+                    Console.WriteLine($"Получает права доступа на время процессора для этого процесса: {process.PrivilegedProcessorTime}");
+                    Console.WriteLine($"Полное время процессора для этого процесса: {process.TotalProcessorTime}");
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------");
+            //2
+            AppDomain domain = AppDomain.CurrentDomain;
+            Console.WriteLine($"Имя: {domain.FriendlyName}");
+            Console.WriteLine($"ID: {domain.Id}");
+            Console.WriteLine($"представляет объект AppDomainSetup и хранит конфигурацию домена приложения: {domain.SetupInformation}");
+            foreach (Assembly assembly in domain.GetAssemblies())
+                Console.WriteLine(assembly.GetName().Name);
+            Console.WriteLine();
+
+            //AppDomain newDomain = AppDomain.CreateDomain("NewDomain1");//новый домен приложения в текущем процессе
+            //foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            //    newDomain.Load(assembly.FullName);//Этот метод применяется для динамической загрузки сборки в текущий домен приложения
+            //AppDomain.Unload(newDomain);//Этот статический метод позволяет выгрузить определенный домен приложения из конкретного процесса
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------");
+            //3
+            ThreadingCounter();
+            //4
+            Thread OddThread = new Thread(odd);
+            Thread EvenThread = new Thread(even);
+            OddThread.Start();
+            EvenThread.Start();
+
+            Thread.Sleep(2200);
+            Console.WriteLine();
+
+            Thread OddThread1 = new Thread(odd);
+            Thread EvenThread1 = new Thread(even);
+            EvenThread1.Priority = ThreadPriority.Highest;
+            OddThread1.Priority = ThreadPriority.Lowest;
+            OddThread1.Start();
+            EvenThread1.Start();
+
+            Thread.Sleep(2200);
+            Console.WriteLine();
+
+            Thread OddThread2 = new Thread(odd);
+            Thread EvenThread2 = new Thread(even);
+            OddThread2.Start();
+            OddThread2.Join();
+            EvenThread2.Start();
+
+            Thread.Sleep(2200);
+            Console.WriteLine();
+
+            Thread OddThread3 = new Thread(odd1);
+            Thread EvenThread3 = new Thread(even1);
+            OddThread3.Start();
+            EvenThread3.Start();
+
+            Thread.Sleep(2200);
+            Console.WriteLine();
+
+            //5
+
+            Timer timer = new Timer((o) => Console.WriteLine(DateTime.Now), null, 0, 1000);
+            Thread.Sleep(5000);
+            timer.Dispose();
+            Console.WriteLine();
+
+
+        }
+
+        //3
+        private static Mutex mutex = new Mutex();
+        public static void ThreadingCounter()
+        {
+            Thread thread1 = new Thread(new ParameterizedThreadStart(Thread1));
+            Thread thread2 = new Thread(new ParameterizedThreadStart(Thread2));
+            Console.Write("Введите число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            thread1.Start(n);
+            thread2.Start(n);
+        }
+        private static void Thread1(object N)
+        {
+            for (int i = 1; i <= (int)N; i++)
+            {
+                mutex.WaitOne();
+                Console.WriteLine("Поток 1 работает: " + i);
+                Console.WriteLine($"Информация о потоке:\n{ThreadInfo(Thread.CurrentThread)}");
+                Thread.Sleep(500);
+                mutex.ReleaseMutex();
+            }
+        }
+        private static void Thread2(object N)
+        {
+            for (int i = 1; i <= (int)N; i++)
+            {
+                mutex.WaitOne();
+                Console.WriteLine("Поток 2 работает: " + i);
+                Console.WriteLine($"Информация о потоке:\n{ThreadInfo(Thread.CurrentThread)}");
+                Thread.Sleep(500);
+                mutex.ReleaseMutex();
+            }
+        }
+        private static string ThreadInfo(Thread thread)
+        {
+            return $"Статус: {thread.ThreadState}\n" +
+                $"Имя: {thread.Name}\n" +
+                $"Приоритет: {thread.Priority}\n" +
+                $"ID: {thread.GetHashCode()}\n" +
+                $"-------------------------------------------";
+        }
+
+        //4
+        static AutoResetEvent AutoReset1 = new AutoResetEvent(true), AutoReset2 = new AutoResetEvent(true);
+        public static void even1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                AutoReset2.WaitOne();
+                Console.WriteLine(i * 2);
+                Thread.Sleep(200);
+                AutoReset1.Set();
+            }
+        }
+        public static void odd1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                AutoReset1.WaitOne();
+                Console.WriteLine(i * 2 - 1);
+                Thread.Sleep(100);
+                AutoReset2.Set();
+            }
+        }
+        public static void even()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * 2);
+                Thread.Sleep(200);
+            }
+        }
+        public static void odd()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * 2 - 1);
+                Thread.Sleep(100);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ ООП/3-й семестр/lab15-OOP/lab15-OOP/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace lab15
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TPL.ParallelVectorByNumber();
+
+            TPL.ParallelVectorByNumberWithTocken();
+            TPL.Continuation();
+            TPL.ParallelFor();
+
+            TPL.Shop();
+            TPL.AsyncDemonstration();
+
+
+
+
+        }
+    }
+    public static class TPL
+    {
+        //1
+        //Умножениевектора размера 100000 на число
+        public static void ParallelVectorByNumber()
+        {
+            Stopwatch stopwatch = new Stopwatch();//Предоставляет набор методов и свойств, которые можно использовать для точного измерения затраченного времени.
+            Random rnd = new Random();
+            int SomeNumber = rnd.Next(20,200);
+            List<Task> Tasks = new List<Task>();
+            BlockingCollection<int> Vector = new BlockingCollection<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                Tasks.Add(new Task(() =>
+                {
+                    for (int i = 0; i < 100000; i++)
+                        Vector.Add(rnd.Next(1, 10000) * SomeNumber);
+                }));
+            }
+            Console.WriteLine(Tasks[0].Status);
+            stopwatch.Start();//Останавливает измерение затраченного времени для интервала.
+            foreach (var task in Tasks)
+                task.Start();
+            Task.WaitAll(Tasks.ToArray());
+            stopwatch.Stop();
+            Console.WriteLine(Tasks[0].Status);
+
+            Console.WriteLine("Количество тактов при использовании Task: " + stopwatch.ElapsedTicks);//Получает общее затраченное время в тактах таймера, измеренное текущим экземпляром.
+        }
+        //2
+        public static void ParallelVectorByNumberWithTocken()
+        {
+            Random rnd = new Random();
+            int SomeNumber = rnd.Next(20, 200);
+            List<Task> Tasks = new List<Task>();
+            List<int> Vector = new List<int>();
+            CancellationTokenSource tokenSource = new CancellationTokenSource();//Отправляет токену CancellationToken сигнал отмены. - этот токен Распространяет уведомление о том, что операции следует отменить.
+
+            for (int i = 0; i < 10; i++)
+            {
+                Tasks.Add(new Task(() =>
+                {
+                    for (int i = 0; i < 1000000; i++) {
+                        //Vector.Add(rnd.Next(1, 10) * SomeNumber);
+                        }
+                }, tokenSource.Token)); //Возвращает объект CancellationToken, связанный с этим объектом CancellationTokenSource.
+
+            }
+
+
+            foreach (var task in Tasks)
+                task.Start(); //Запускает задачу Task, планируя ее выполнение в текущем планировщике TaskScheduler.
+            tokenSource.Cancel();//Передает запрос на отмену.
+            Console.WriteLine("Status: " + Tasks[5].Status);//Получает состояние TaskStatus данной задачи.
+        }
+
+        //3-4
+        public static void Continuation()
+        {
+            Task<double> task1 = Task.Run(() => { return Math.Pow(2, 16); }); //Ставит в очередь заданную работу для запуска в пуле потоков и возвращает объект Task, представляющий эту работу.
+            Task<double> task2 = task1.ContinueWith(x => { return Math.Pow(264, 5) * x.Result; });//Создает продолжение, которое выполняется асинхронно после завершения выполнения целевой задачи Task.
+            Task task3 = task2.ContinueWith(x => { Console.WriteLine("Ответ: " + x.Result * 12); });
+            task3.GetAwaiter().GetResult();//GetAwaiter().GetResult(); — возврат результата / выброс оригинального исключения — служебный метод компилятора, поэтому использовать его не рекомендуется. Используется механизмом async/await.
+        }
+
+        //5
+        public static void ParallelFor()
+        {
+            BlockingCollection<int> Arr = new BlockingCollection<int>();
+            Random rnd = new Random();
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            //       For(int, int,          Action<int>)   Первый параметр метода задает начальный индекс элемента в цикле, а второй параметр - конечный индекс.Третий параметр -делегат Action - указывает на метод, который будет выполняться один раз за итерацию:
+            Parallel.For(0, 1000000, i => { Arr.Add(rnd.Next(1, 1000)); }); //Метод Parallel.For позволяет выполнять итерации цикла параллельно. Он имеет следующее определение:
+            stopwatch.Stop();
+
+            Console.WriteLine("Parallel.For: \t" + stopwatch.ElapsedTicks + " тактов");
+
+            stopwatch.Reset();//Останавливает измерение интервала времени и обнуляет затраченное время.
+            stopwatch.Start();//Запускает или возобновляет измерение затраченного времени для интервала.
+            List<int> Array = new List<int>();
+            for (int i = 0; i < 1000000; i++)
+                Array.Add(rnd.Next(1, 1000));
+            stopwatch.Stop();
+
+            Console.WriteLine("For:\t\t" + stopwatch.ElapsedTicks + " тактов");
+        }
+        //6
+        public static void ParallelInvoke()//Выполняет все предоставленные действия, по возможности в параллельном режиме.
+        {
+            Parallel.Invoke(() => { Console.WriteLine("First function"); }, () => { Console.WriteLine("Second function"); }, () => { Console.WriteLine("Third function"); });
+            //Метод Parallel.Invoke в качестве параметра принимает массив объектов Action, то есть мы можем передать в данный метод набор методов, которые будут вызываться при его выполнении.
+        }
+
+        //7
+        public static void Shop()
+        {
+            Dictionary<int?, string> Product = new Dictionary<int?, string>();
+            Product.Add(0, "Диван");
+            Product.Add(1, "Стул");
+            Product.Add(2, "Стол");
+            Product.Add(3, "Постер");
+            Product.Add(4, "Табурет");
+            Product.Add(5, "Ленолиум");
+            Product.Add(6, "Кровать");
+            Product.Add(7, "Обои");
+            Product.Add(8, "Люстра");
+            Product.Add(9, "Окно");
+
+            BlockingCollection<int> Warehouse = new BlockingCollection<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Task.Run(() =>
+                {
+                    Random rnd = new Random();
+                    while (true)
+                    {
+                        Thread.Sleep(rnd.Next(1000, 5000));
+
+                        int Counter = 0;
+                        Warehouse.Add(rnd.Next(0, 9));
+                        Console.WriteLine("Склад: ");
+                        foreach (var product in Warehouse)
+                        {
+                            Console.WriteLine(Product[product]);
+                            Counter++;
+                        }
+                        if (Counter == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Ничего");
+                            Console.ResetColor();
+                        }
+
+                        Console.WriteLine("-------------------");
+                        Thread.Sleep(rnd.Next(1000, 5000));
+                    }
+                });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() =>
+                {
+                    Random rnd = new Random();
+                    while (true)
+                    {
+                        Thread.Sleep(rnd.Next(1000, 5000));
+                        foreach (var product in Warehouse)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(Product[product] + " куплен");
+                            Console.ResetColor();
+                            Console.WriteLine("-------------------");
+
+                            int i = product;
+                            int Counter = 0;
+                            Warehouse.TryTake(out i);
+                            Console.WriteLine("Склад: ");
+                            foreach (var productt in Warehouse)
+                            {
+                                Console.WriteLine(Product[productt]);
+                                Counter++;
+                            }
+                            if (Counter == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine("Ничего");
+                                Console.ResetColor();
+
+                            }
+                            else
+                                Counter = 0;
+                            Console.WriteLine("-------------------");
+                            break;
+                        }
+                    }
+                });
+            }
+            Thread.Sleep(5000);
+        }
+
+        //8
+        public static void Print()
+        {
+           // Thread.Sleep(3000);     // имитация продолжительной работы
+            Console.WriteLine("Hello METANIT.COM");
+        }
+        //оператор await можно применять только в методе, который имеет модификатор async.
+        public static async void AsyncDemonstration()
+        {
+            await Task.Run(() => Print());
+        }
+    }
+}
